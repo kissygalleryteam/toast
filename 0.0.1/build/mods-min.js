3c0d2b9be1c5d9a08f3440746cb4cbbc3c48a68b
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-16 17:29:41 +0800

    sync daily version 0.0.1

---
+++ 0.0.1/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-08-16 5:25:35 PM*/
+KISSY.config("modules",{"kg/toast/0.0.1/index":{requires:["node","base","/Users/admin/AliDrive/proj/toast/index.css"]}});
---
+++ 0.0.1/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/toast/0.0.1/index': { requires: ['node', 'base', '/Users/admin/AliDrive/proj/toast/index.css']} 
+});
